d20bcae79191fcd940224fb432caf649fb241669
viniciuspsf <viniciussilvapsf97@gmail.com>
2026-06-08 17:56:34 -0300

    alteração

---
+++ Criaçao de Site/app-servers.js
@@ -0,0 +1,25 @@
+// LOGIN
+document.getElementById("loginForm")?.addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const response = await fetch("server-side/login.php", {
+        method: "POST",
+        body: new FormData(e.target)
+    });
+
+    const result = await response.text();
+    document.getElementById("message").innerText = result;
+});
+
+// REGISTRO
+document.getElementById("registerForm")?.addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const response = await fetch("server-side/register.php", {
+        method: "POST",
+        body: new FormData(e.target)
+    });
+
+    const result = await response.text();
+    document.getElementById("message").innerText = result;
+});
